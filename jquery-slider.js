22cfe50666028edb1eb105f071d075afb795f527
Svyatoslav Reyentenko <rsvato@gmail.com>
2013-08-30 03:00:10 +0400

    Changed to JQuery UI widget

--- jquery-slider.js
+++ jquery-slider.js
@@ -1,138 +1,187 @@
 (function($){
-	var settings; 
-	$.fn.slider = function(sliderSettings) {
-		settings = $.extend({
-			sliderClass: 'slider',
-			wrapperClass: 's-content-wrapper',
-			scrollableClass: 'scrollable',
-			rows: 1,
-			element: 'div'
-		}, sliderSettings || {});
-		
-		var firstElement = function(elem) {
-			var examine = elem || $content;
-			var result = examine.find(settings.element).first();
-			return result;
-		};
-		
-		var lastElement = function(elem) {
-			var examine = elem || $content;
-			var result = examine.find(settings.element).last();
-			return result;
-		};
-		
-		var widthAndHeight = function(container) {
-			var first = firstElement(container);
-			var elementWidth = first.outerWidth(true);
-			var elementHeight = first.outerHeight(true);
-			var height = $content.outerHeight();
-			var height = elementHeight * settings.rows;
-			var hasGaps = 'inline-block' === first.css('display');
-			var addition = 0;
-			var fontSize = parseInt(first.css('font-size'));
-			if (hasGaps && fontSize) {
-				addition = fontSize;
-			}
-			var total = (elementWidth + addition) * $content.children().length;
-			var dimensions = {width: total, height: height, itemWidth: elementWidth + addition};
-			if (total > $(window).width()) {
-				total = Math.floor(total / settings.rows);
-				dimensions.width = total;
-			}
-			return dimensions;
-		}
-		
-		var $content = $(this);
-		var restore = false;
-		var elem = $content;
-		var restore = false;
-		if ($content.is(":hidden")) {
-			elem = $content.clone().attr('style', 'position: absolute !important; top: -1000 !important;').attr('id', 'slider_fix_clone');
-			elem.css('display', 'block');
-			elem.prependTo('body');
-		}
-		var dimensions = widthAndHeight(elem);
-		if (restore) {
-			$('#slider_fix_clone').remove();
-		}
-		$content.css('width', dimensions.width);
-		$content.css('margin', '0 0');
-		var $slider = $('<div class="slider"/>');
-		var $wrapper = $('<div class="s-content-wrapper"/>');
-		var $scrollable = $('<div class="scrollable"/>');
-		$wrapper.append($scrollable);
-		$scrollable.append($content);
-		var animating = false;
-		
-		var visibleCond = function(elem, f) {
-			return f(elem);
-		};
-		
-		var checkScrollVisibility = function() {
-			var rightmost = settings.rows == 1 ? lastElement() : function(list) {
-				index = Math.ceil(list.length / settings.rows);
-				return list[index - 1];
-			}($content.children());
-			var left = visibleCond($(rightmost), function(e) {
-				var lft = e.offset().left;
-				if (lft === 0 && $content.children().length > 0) {
-					lft = dimensions.width;
-				}
-				var rightEdge = $(window).width() - (lft + e.outerWidth());
-				return rightEdge > 0;
-			});
-			var right = visibleCond(firstElement(), function(e) {
-				return e.offset().left > 0;
-			});
-			return {left: !left, right: !right};
-		};
-		
-		var makeScroll = function(direction, target) {
-			var sign = '+', symbol = '>'
-			if (direction === 'left') {
-				sign = '-';
-				symbol = '<'
-			}
-			var link = $('<div rel="' + direction + '" class="scroll-' + direction + '"/>');
-			link.css('height', dimensions.height);
-			link.on('mouseenter', function(evt){
-				evt.preventDefault();
-				var content = $(target).children().first();
-				var target = $(evt.target);
-				var direction = target.attr('rel');
-				animating = true;
-				offset = dimensions.itemWidth;
-				$content.animate({marginLeft: sign + '=' + offset}, 1000, function(){
-					var visibility = checkScrollVisibility();
-					$slider.find('.scroll-left').toggle(showOrHide = visibility.left);
-					$slider.find('.scroll-right').toggle(showOrHide = visibility.right);
-					if (animating && direction === 'right' && visibility.right) {
-						target.trigger('mouseenter');
-					}
-					if (animating && direction === 'left' && visibility.left) {
-						target.trigger('mouseenter');
-					}
-				});
-				
-			});
-			
-			link.on('mouseout', function(evt) {
-				animating = false;
-			});
-			
-			if (direction === 'right') {
-				link.css('left', ($(window).width() - 70) + "px")
-			}
-			return link;
-		}
-		
-		$slider.append(makeScroll('left', $wrapper));
-		$slider.append($wrapper);
-		$slider.append(makeScroll('right', $wrapper));
-		var visibility = checkScrollVisibility();
-		if (!visibility.left) {
-			$slider.find('.scroll-left').css('display', 'none');
-		}
-		return $slider;
-	};
+  var settings;
+  $.widget('ui.slider', {
+    options: {
+      sliderClass: 'slider',
+      wrapperClass: 's-content-wrapper',
+      scrollableClass: 'scrollable',
+      rows: 1,
+      element: 'div',
+      widthToWindow: true
+    },
+    animating: 0,
+    fxWidth: function() {},
+    width: 0,
+    _create: function() {
+      settings = $.extend({}, this.options || {});
+      var _self = this;
+      var $parent = this.element;
+      var resizeTimer;
+      this.width = $parent.outerWidth(true);
+      if (settings.widthToWindow) {
+        this.width = $(window).width();
+      }
+      var koeff = this.width / $(window).width();
+      this.fxWidth = function() {
+        return $(window).width() * koeff;
+      } ;
+      $parent.css('overflow', 'hidden');
+      $parent.css('width', this.width + "px");
+      var $content = $parent.children().first().detach();
+      this.$content = $content;
+      $content.find('style').remove();
+      var elem = $content;
+      var restore = false;
+      if ($content.is(":hidden")) {
+        elem = $content.clone().attr('style', 'position: absolute !important; top: -1000 !important;').attr('id', 'slider_fix_clone');
+        elem.css('display', 'block');
+        elem.prependTo('body');
+        restore = true;
+      }
+      var dimensions = this.widthAndHeight(elem);
+      if (restore) {
+        $('#slider_fix_clone').remove();
+      }
+      $content.css('width', dimensions.width);
+      $content.css('margin', '0 0');
+      var $slider = $('<div class="slider"/>');
+      var $wrapper = $('<div class="s-content-wrapper"/>');
+      $wrapper.append($content);
+
+      $slider.append(this.makeScroll('left', dimensions.height))
+        .append($wrapper)
+        .append(this.makeScroll('right', dimensions.height));
+
+      $(window).on('resize', function() {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(_self.adjustWidthAndPosition(), 100);
+      });
+
+      this.element.on('change-scroller', function(event, data) {
+        var leftScroll = $slider.find('.scroll-left');
+        leftScroll.toggle(data.left);
+        var rightScroll = $slider.find('.scroll-right');
+        rightScroll.toggle(data.right);
+        if (_self.animating != 0) {
+          if ((_self.animating < 0 && !data.right) || (_self.animating > 0 && !data.left)) {
+            _self.stopAnimation();
+          } else {
+            _self.animate(_self.animating);
+          }
+        }
+      });
+
+      $content.on('slide', function(event, direction){
+        $content.animate({marginLeft: '+=' + direction * dimensions.itemWidth}, 1000, function(){
+          _self.checkScrollVisibility(_self.element, _self.$content, dimensions);
+        })
+      });
+
+      this.checkScrollVisibility($parent, $content, dimensions);
+      $slider.appendTo($parent);
+      return $parent;
+    },
+
+    firstElement: function(elem) {
+      var examine = elem || this.$content;
+      return examine.find(this.options.element).first();
+    },
+
+    elementCount: function(elem) {
+      var examine = elem || this.$content;
+      return examine.children(this.options.element).length;
+    },
+
+    widthAndHeight: function(container) {
+      var first = this.firstElement(container);
+      var elementWidth = first.outerWidth(true);
+      var elementHeight = first.outerHeight(true);
+      var hasGaps = true;
+      var addition = 0;
+      var fontSize = parseInt(first.css('font-size'));
+      if (hasGaps && fontSize) {
+        addition = fontSize;
+      }
+      elementWidth = elementWidth + addition;
+      var total = (elementWidth + addition) * this.elementCount(container);
+      var dimensions = {width: total, height: elementHeight, itemWidth: elementWidth};
+      if (total > $(window).width()) {
+        var longestRow = Math.floor(this.elementCount(container) / settings.rows);
+        total = elementWidth * longestRow;
+        while (total < ($(window).width() - elementWidth)) {
+          longestRow += 1;
+          total = elementWidth * longestRow;
+        }
+        dimensions.width = total;
+        dimensions.height = elementHeight * settings.rows
+      }
+      return dimensions;
+    },
+
+    checkScrollVisibility: function($parent, $content, dimensions) {
+      var right = function() {
+        var width = $parent.outerWidth(false);
+        var right = (dimensions.width + parseInt($content.css('marginLeft')));
+        return right > width;
+      };
+      var left = function() {
+        return parseInt($content.css('marginLeft')) < 0;
+      };
+      this.element.trigger('change-scroller', {left: left.call(), right: right.call()});
+    },
+
+    animate: function(dir) {
+      this.animating = dir;
+      this.$content.trigger('slide', dir);
+    },
+
+    stopAnimation: function() {
+      this.animating = 0;
+    },
+
+    makeScroll: function(direction, height) {
+      var sign = '+';
+      if (direction === 'right') {
+        sign = '-';
+      }
+      var link = $('<div rel="' + direction + '" class="scroll-' + direction + '"/>');
+      link.css('height', height);
+      link.on('click', function(evt) {
+        evt.preventDefault();
+        evt.stopPropagation();
+      });
+      var self = this;
+      link.on('mouseenter', function(evt){
+        evt.preventDefault();
+        var target = $(evt.target);
+        var direction = target.attr('rel');
+        var coeff = direction === 'right' ? -1 : 1;
+        self.animate(coeff);
+      });
+
+      link.on('mouseout', function() {
+        self.stopAnimation();
+      });
+
+      if (direction === 'right') {
+        var leftMargin = this.width - 63;
+        link.css('left', leftMargin + "px");
+      }
+      return link;
+    },
+
+    adjustWidthAndPosition: function($parent, $slider) {
+      var width = Math.floor(this.fxWidth());
+      $parent.attr('style', 'width: ' + width + 'px !important');
+      var rightPosition = width - 63;
+      var rightScroll = $slider.find('div.scroll-right');
+      rightScroll.css('left', rightPosition + 'px');
+      var visibility = this.checkScrollVisibility($parent, this.$content, this.widthAndHeight(this.$content));
+      if (visibility.left) {
+        $slider.find('.scroll-left').css('display', 'block');
+      } else {
+        $slider.find('.scroll-left').css('display', 'none');
+      }
+    }
+  })
 })(jQuery);
